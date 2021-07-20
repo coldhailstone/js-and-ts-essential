a3b9f611e82b1fd34378ceef3124d30face22cd5
cold_hailstone <cksn1993@gmail.com>
2021-07-20 15:37:57 +0900

    12줄의 코드로 시작하기

---
+++ HN-client/app.js
@@ -0,0 +1,14 @@
+const ajax = new XMLHttpRequest();
+
+ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
+
+for (let i = 0; i < newsFeed.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = newsFeed[i].title;
+    ul.appendChild(li);
+}
+document.getElementById('root').appendChild(ul);
