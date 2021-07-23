c153fa648979232d50d0edf83f44056d9568a797
cold_hailstone <cksn1993@gmail.com>
2021-07-23 15:56:14 +0900

    CH03_09. 상태를 가져보자, 읽은 글 표시하기

--- HN-client/app.js
+++ HN-client/app.js
@@ -4,7 +4,8 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
-    currentPage: 1
+    currentPage: 1,
+    feeds: []
 };
 
 function getData(url) {
@@ -14,8 +15,16 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
+function makedFeeds(feeds) {
+    for (let i = 0; i < feeds.length; i++) {
+        feeds[i].read = false;
+    }
+
+    return feeds;
+}
+
 function newsFeed() {
-    const newsFeed = getData(NEWS_URL);
+    let newsFeed = store.feeds;
     const newsList = [];
     let template = `
         <div class="bg-gray-600 min-h-screen">
@@ -41,6 +50,10 @@ function newsFeed() {
             </div>
         </div>
     `;
+
+    if (newsFeed.length === 0) {
+        newsFeed = store.feeds = makedFeeds(getData(NEWS_URL));
+    }
 
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         if (!newsFeed[i]) break;
@@ -104,6 +117,13 @@ function newsDetail() {
         </div>
     `;
 
+    for (let i = 0; i < store.feeds.length; i++) {
+        if (store.feeds[i].id === Number(id)) {
+            store.feeds[i].read = true;
+            break;
+        }
+    }
+
     function makeComment(comments, called = 0) {
         const commentString = [];
     
@@ -135,7 +155,7 @@ function router() {
     if (routePath == '') {
         newsFeed();
     } else if (routePath.indexOf('#/page/') >= 0) {
-        store.currentPage = parseInt(routePath.substr(7));
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
