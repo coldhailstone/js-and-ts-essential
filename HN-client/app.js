835a49376bc17b53209ce0c4e35c39072fbc40e7
cold_hailstone <cksn1993@gmail.com>
2021-07-23 14:42:35 +0900

    CH03_05. 라우터? 화면 처리기 만들기

--- HN-client/app.js
+++ HN-client/app.js
@@ -11,29 +11,47 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-window.addEventListener('hashchange', () => {
-    const id = location.hash.substr(1);
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
+    newsList.push('<ul>');
+    for (let i = 0; i < newsFeed.length; i++) {
+        newsList.push(`
+            <li>
+                <a href="#${newsFeed[i].id}">
+                    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+                </a>
+            </li>
+        `);
+    }
+    newsList.push('</ul>');
 
-    title.innerText = newsContent.title;
-    content.appendChild(title);
-});
-
-for (let i = 0; i < newsFeed.length; i++) {
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <li>
-            <a href="#${newsFeed[i].id}">
-                ${newsFeed[i].title} (${newsFeed[i].comments_count})
-            </a>
-        </li>
-    `;
-    ul.appendChild(div.firstElementChild);
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substr(1);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+    
+    if (routePath == '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
